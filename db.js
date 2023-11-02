ca8a0ebd61e8bf94f6fa9e746288300d81f284b4
Kumar Rohan <rohansah1997.29@gmail.com>
2023-11-02 14:59:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 require("dotenv").config()
 
-const connection=mongoose.connect(process.env.mongoURL)
+const connection=mongoose.connect("mongodb+srv://00rohansah00kr:rohan%22rj07@cluster0.ydojkd9.mongodb.net/blogapp?retryWrites=true&w=majority")
 
 
 module.exports={
